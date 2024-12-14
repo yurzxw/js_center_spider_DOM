acfc339cf2ee2b486c452450464d328d1e75695a
Yura <yuradziavoruk5@gmail.com>
2024-12-14 16:30:04 +0200

    make build

---
+++ index.3afb5a1d.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider");e.style.position="absolute",t.style.position="relative",t.style.transform="translate(-50%, -50%)",t.style.top="50%",t.style.left="50%";
+//# sourceMappingURL=index.3afb5a1d.js.map
